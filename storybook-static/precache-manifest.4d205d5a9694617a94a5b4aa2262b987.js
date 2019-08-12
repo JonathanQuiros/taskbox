173ae0868d04cbd80ae179eaebf193f186443272
Jonathan Quiros <jonathanquirosp@gmail.com>
2019-08-12 16:12:13 -0700

    Taskbox UI

---
+++ storybook-static/precache-manifest.4d205d5a9694617a94a5b4aa2262b987.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "365dcb331e7667d75498a86212e862a2",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "e5c6e6be1c2d80ac8f3c",
+    "url": "main.css"
+  },
+  {
+    "url": "main.f94bc69657510c18a998.bundle.js"
+  },
+  {
+    "url": "runtime~main.f94bc69657510c18a998.bundle.js"
+  },
+  {
+    "url": "vendors~main.f94bc69657510c18a998.bundle.js"
+  }
+]);
